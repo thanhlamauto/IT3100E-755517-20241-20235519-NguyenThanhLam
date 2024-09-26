6a9a9e10c3d4adbf9be653b43c5368e49c1b088a
Thanh Lâm Nguyễn <thanhlamtba@gmail.com>
2024-09-26 15:58:02 +0700

    Complete 2.2.1

--- lab01/HelloWorld.java
+++ lab01/HelloWorld.java
@@ -1,5 +1,7 @@
+package lab01;
 public class HelloWorld {
     public static void main (String[] args) {
-        System.out.println("Hello, world");
+        System.out.println("Xin chao \n cac ban");
+        System.out.println("Hello \t world");
     }
 }
