b83a6d1ec14cccaab47acc9758681159b2e5fba4
Thanh Lâm Nguyễn <thanhlamtba@gmail.com>
2024-10-31 15:28:59 +0700

    finish lab02

---
+++ lab02/AimsProject/Cart.java
@@ -0,0 +1,47 @@
+public class Cart {
+
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	private int qtyOrdered = 0;
+
+ // Method to add a DigitalVideoDisc to the cart
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered <  itemsOrdered.length) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++; // Increment qtyOrdered when a disc is added
+        } else {
+            System.out.println("Cart is full!");
+        }
+    }
+
+    // Method to remove a DigitalVideoDisc from the cart
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if ( itemsOrdered[i].equals(disc)) {
+                // Shift discs to fill the gap
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] =  itemsOrdered[j + 1];
+                }
+                // itemsOrdered[qtyOrdered - 1] = null; // Clear the last slot
+                qtyOrdered--; // Decrement qtyOrdered
+                break;
+            }
+        }
+    }
+
+    public float totalCost(){
+        float sum_cost = 0;
+        for (int i = 0; i < qtyOrdered; i++){
+            sum_cost += itemsOrdered[i].getCost();
+        }
+        return sum_cost;
+    }
+
+    public int getQtyOrdered() {
+		return qtyOrdered;
+	}
+}
+        
+
+	
+		
---
+++ lab02/AimsProject/DigitalVideoDisc.java
@@ -0,0 +1,57 @@
+
+public class DigitalVideoDisc {
+    private String title;
+	private String category;
+	private String directory;
+	private int length;
+	private float cost;
+
+	public DigitalVideoDisc(String title, String category, String directory, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.directory = directory;
+		this.length = length;
+		this.cost = cost;
+	}
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.directory = "Unknown";
+		this.length = -1;
+		this.cost = cost;
+	}
+
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirectory() {
+		return directory;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+    public boolean equals(DigitalVideoDisc another_disc){
+        if (!title.equals(another_disc.getTitle())) return false;
+        if (!category.equals(another_disc.getCategory())) return false;
+        if (!directory.equals(another_disc.getDirectory())) return false;
+        if (length != another_disc.getLength()) return false;
+        if (cost != another_disc.getCost()) return false;
+        return true;
+
+    }
+
+}
